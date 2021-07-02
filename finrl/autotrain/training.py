1ac16e4a84afa3b15edd6b2aa2b7d8041adbf7f5
NhatTan <susannoo123@gmail.com>
2021-07-02 16:35:11 +0700

    define training esemble agent

--- finrl/autotrain/training.py
+++ finrl/autotrain/training.py
@@ -14,6 +14,7 @@ from finrl.preprocessing.preprocessors import FeatureEngineer
 from finrl.preprocessing.data import data_split
 from finrl.env.env_stocktrading import StockTradingEnv
 from finrl.model.models import DRLAgent
+from finrl.model.models import DRLEnsembleAgent
 from finrl.trade.backtest import backtest_stats, backtest_plot, get_daily_return, get_baseline
 
 import itertools
@@ -108,3 +109,82 @@ def train_one():
     perf_stats_all = backtest_stats(df_account_value)
     perf_stats_all = pd.DataFrame(perf_stats_all)
     perf_stats_all.to_csv("./" + config.RESULTS_DIR + "/perf_stats_all_" + now + ".csv")
+
+
+# Define train_ensemble_agent()
+def train_ensemble_agent():
+    print("==============Start Fetching Data===========")
+    df = YahooDownloader(
+        start_date=config.START_DATE,
+        end_date=config.END_DATE,
+        ticker_list=config.DOW_30_TICKER,
+    ).fetch_data()
+    print("==============Start Feature Engineering===========")
+    fe = FeatureEngineer(
+        use_technical_indicator=True,
+        tech_indicator_list=config.TECHNICAL_INDICATORS_LIST,
+        use_turbulence=True,
+        user_defined_feature=False,
+    )
+
+    processed = fe.preprocess_data(df)
+
+    list_ticker = processed["tic"].unique().tolist()
+    list_date = list(pd.date_range(processed['date'].min(),processed['date'].max()).astype(str))
+    combination = list(itertools.product(list_date,list_ticker))
+
+    processed_full = pd.DataFrame(combination,columns=["date","tic"]).merge(processed,on=["date","tic"],how="left")
+    processed_full = processed_full[processed_full['date'].isin(processed['date'])]
+    processed_full = processed_full.sort_values(['date','tic'])
+
+    processed_full = processed_full.fillna(0)
+
+    # Training & Trading data split
+    train = data_split(processed_full, config.START_DATE, config.START_TRADE_DATE)
+    trade = data_split(processed_full, config.START_TRADE_DATE, config.END_DATE)
+
+    # calculate state action space
+    stock_dimension = len(train.tic.unique())
+    state_space = (
+        1
+        + 2 * stock_dimension
+        + len(config.TECHNICAL_INDICATORS_LIST) * stock_dimension
+    )
+
+    train_period = ['2009-01-01', '2015-10-01']
+    val_test_period = ['2015-10-01', '2016-01-01']
+
+    env_kwargs = {
+        "df": df,
+        "train_period" : train_period,
+        "val_test_period" : val_test_period,
+        "rebalance_window" : 63,
+        "validation_window" : 63,
+        "stock_dim" : stock_dimension,
+        "hmax" : 100 ,
+        "initial_amount" : 1000000,
+        "buy_cost_pct" : 0.001,
+        "sell_cost_pct" : 0.001,
+        "reward_scaling" : 1e-4,
+        "state_space" : state_space,
+        "action_space" : stock_dimension,
+        "tech_indicator_list" : config.TECHNICAL_INDICATORS_LIST,
+        "print_verbosity" : 10
+        }
+
+    timesteps_dict = {
+        'a2c' : 100000,
+        'ppo' : 100000,
+        'ddpg' : 50000
+    }
+
+    # e_train_gym = StockTradingEnv(df=train, **env_kwargs)
+    # env_train, _ = e_train_gym.get_sb_env()
+    ensembleAgent = DRLEnsembleAgent(**env_kwargs)
+
+    print("==============Model Training===========")
+    now = datetime.datetime.now().strftime("%Y%m%d-%Hh%M")
+    ensemble_model = ensembleAgent.run_ensemble_strategy(config.A2C_PARAMS, config.PPO_PARAMS, config.DDPG_PARAMS, timesteps_dict=timesteps_dict)
+
+    training_ensemble_agent = ensembleAgent.run_ensemble_strategy(
+                a2c_model, ppo_model, ddpg_model, timesteps_dict=100000)
