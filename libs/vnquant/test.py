23a8921395b0d6ecdb9a751fd84c6ef435036a05
ThangDuong59 <60088921+ThangDuong59@users.noreply.github.com>
2021-07-02 20:01:48 +0700

    delete files

--- libs/vnquant/test.py
+++
@@ -1,5 +0,0 @@
-# Test data loader
-import vnquant.DataLoader as dl
-loader = dl.DataLoader('VND', '2018-02-02','2018-04-02', data_source='VND', minimal=True)
-data = loader.download()
-print(data.head())
